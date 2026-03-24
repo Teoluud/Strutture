aeaf3b88786670cd68bc7e88319735521aa6c64e
Teoluud <texwiller2004@gmail.com>
2026-03-25 00:38:20 +0100

    first real commit

--- fotocond.py
+++ fotocond.py
@@ -1,23 +1,20 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
-
-def read_file(input_file: str) -> pd.DataFrame:
-    """ Imports the TSV into a dataframe.
-    """
-    df: pd.DataFrame = pd.read_csv(input_file, sep='\t')
-    return df
-
+from calibration import *
 
 class DataProcessing:
     """ Processes and elaborates the data.
     """
 
-    def __init__(self, input_file: str, y_column: str, y_err_column: str) -> None:
-        self.data_frame: pd.DataFrame   = read_file(input_file)
-        self.x: np.ndarray              = self.data_frame['Un arbitr'].to_numpy()
-        self.y: np.ndarray              = self.data_frame[y_column].to_numpy()
-        self.y_err: np.ndarray          = self.data_frame[y_err_column].to_numpy()
+    def __init__(self) -> None:
+        """ Constructor.
+        """
+        pass
+
+    def config(self, x_column: str, y_column: str, y_err_column: str) -> None:
+        """ Defines the x and y arrays.
+        """
+        self.x: np.ndarray      = self.data_frame[x_column].to_numpy()
+        self.y: np.ndarray      = self.data_frame[y_column].to_numpy()
+        self.y_err: np.ndarray  = self.data_frame[y_err_column].to_numpy()
 
 
 class Display(DataProcessing):
@@ -39,7 +36,13 @@ class Display(DataProcessing):
 
 
 if __name__ == "__main__":
-    fotod = DataProcessing("data.txt", 'Vpp fotodiodo', 'Err vpp fotodiodo')
-    print(fotod.data_frame)
-    plt.scatter(fotod.x, fotod.y)
-    plt.show()
+
+    R: float = 1e6
+    ERR_R: float = 0.05*R
+
+    data_df     = read_file('data.csv')
+    ref_df      = read_file('reference.csv')
+    merged_df   = pd.merge(data_df, ref_df, on='un arb', suffixes=('_sample','_ref'))
+    # Calculate transmittance
+    merged_df['transmittance'] = merged_df['vpp fotod_sample'] / merged_df['vpp fotod_ref']
+    print(merged_df)

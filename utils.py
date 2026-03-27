69359c4be03d4fd2a9d82f57393fee12f741f7e0
Teoluud <texwiller2004@gmail.com>
2026-03-27 09:51:07 +0100

    add generic data analysis class

--- utils.py
+++ utils.py
@@ -1,9 +1,54 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import ROOT
 
 def read_file(input_file: str) -> pd.DataFrame:
-    """ Imports the TSV into a dataframe.
+    """ Imports the CSV into a dataframe.
     """
     df: pd.DataFrame = pd.read_csv(input_file)
     return df
+
+class DataAnalysis:
+    """ Class for generic data analysis methods.
+    """
+
+    def __init__(self, dataframe: pd.DataFrame) -> None:
+        """ Constructor.
+        """
+        self.df: pd.DataFrame = dataframe
+        self.graphs = []
+
+    def make_plot(self, x_col:str, y_col: str, ex_col:str, ey_col: str, title: str, xlabel: str, ylabel: str) -> ROOT.TGraphErrors:
+        """ Creates the graph and returns it.
+        """
+        x: np.ndarray   = self.df[x_col].to_numpy(dtype=np.float64)
+        y: np.ndarray   = self.df[y_col].to_numpy(dtype=np.float64)
+        ex: np.ndarray  = self.df[ex_col].to_numpy(dtype=np.float64)
+        ey: np.ndarray  = self.df[ey_col].to_numpy(dtype=np.float64)
+        n: int          = len(x)
+        # Create the graph
+        graph: ROOT.TGraphErrors = ROOT.TGraphErrors(n, x, y, ex, ey)
+        graph.SetTitle(f"{title};{xlabel};{ylabel}")
+        graph.SetMarkerStyle(21)
+        graph.SetMarkerColor(ROOT.kBlue + 2)
+        self.graphs.append(graph)
+        return graph
+    
+    def make_simple_plot(self, x_col: str, y_col: str, title: str, xlabel: str, ylabel: str) -> ROOT.TGraph:
+        """ Creates a TGraph (without error bars).
+        """
+        x: np.ndarray   = self.df[x_col].to_numpy(dtype=np.float64)
+        y: np.ndarray   = self.df[y_col].to_numpy(dtype=np.float64)
+        n: int          = len(x)
+        graph: ROOT.TGraph = ROOT.TGraph(n, x, y)
+        graph.SetTitle(f"{title};{xlabel};{ylabel}")
+        graph.SetMarkerStyle(21)
+        graph.SetMarkerColor(ROOT.kBlue + 2)
+        self.graphs.append(graph)
+        return graph
+    
+    def display(self, canvas_name: str, graph) -> None:
+        canvas = ROOT.TCanvas(canvas_name, canvas_name, 1000, 800)
+        canvas.cd()
+        graph.Draw("AP")
